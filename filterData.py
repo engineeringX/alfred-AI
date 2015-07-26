cfa1beb09eb8549e64fb5582a7f3870f819fb503
Simarpreet Singh <simar@linux.com>
2015-07-25 22:47:32 -0400

    adding threshold filter window logic

--- filterData.py
+++ filterData.py
@@ -1,7 +1,13 @@
 import math
 import argparse
+import collections
+
+from collections import deque
 
 IMU_data = "/home/simar/parse/inputFolder/imu.dat"
+FALL_THRESH = 0.8
+
+lines_fifo = deque()
 
 def init():
     parser = argparse.ArgumentParser(description='input filter for IMU data')
@@ -23,12 +29,12 @@ def init():
 
 def main(args):
     weights = []
-    M = args.filterLength - 1
+    M = args.filterLength
     M_PI = math.pi
     ft1 = args.firstCutOff / args.samplingFreq
     ft2 = args.secondCutOff / args.samplingFreq
 
-    for sample in xrange(1, args.filterLength):
+    for sample in xrange(0, args.filterLength):
         if (sample != M / 2):
             weights.insert(sample, math.sin(2*M_PI*ft2*(sample-M/2)/(M_PI*(sample-M/2)-math.sin(2*M_PI*ft1*(sample-M/2))/(M_PI*(sample-M/2)))))
         else:
@@ -36,6 +42,22 @@ def main(args):
         weights.insert(sample, weights[sample]*(0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
 
     outputSignal = 0
+
+    fd = open(IMU_data, 'r')
+
+    for line in open(IMU_data, 'r'):
+        if len(lines_fifo) == args.filterLength:
+            for sample in xrange(0, args.filterLength):
+                outputSignal += lines_fifo[sample]
+
+            if outputSignal >= FALL_THRESH:
+            # push parse notification
+
+            lines_fifo.popleft()
+            lines_fifo.append(line.split(',')[2])
+        else:
+            lines_fifo.append(line.split(',')[2])
+
 
 if __name__ == "__main__":
     init()
