66182fac029b9d7a6d0660557aabc0a6c98c31c0
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2016-02-02 20:16:29 -0500

    Update packets.py

--- packets.py
+++ packets.py
@@ -3,6 +3,7 @@ import sys
 f = open(sys.argv[1], 'r')
 
 missed = 0
+true_count = 0
 count = int(f.readline().split(',')[6])
 f.seek(0, 0)
 lines = f.readlines()
@@ -10,10 +11,11 @@ for line in lines:
   id = int(line.split(',')[6])
   missed += (id - count)
   print id, count, missed
+  true_count += 1
   if (id - count) == 0:
     count += 1
   else:
     count = id + 1
 
-print 'missed: %d, count: %d' % (missed, count)
+print 'missed: %d, count: %d' % (missed, true_count)
 f.close()
