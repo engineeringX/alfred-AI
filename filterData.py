87167c991146a99a0159b1b8d8a0dfa6b699db7d
Simarpreet Singh <simar@opendns.com>
2016-02-23 20:32:14 -0500

    filter: Adding a second filter for new algorithm
    Signed-off-by: Simarpreet Singh <simar@opendns.com>

--- filterData.py
+++ filterData.py
@@ -11,13 +11,17 @@ connection.connect()
 
 IMU_data = "./data/test2"
 filterLength = 50
-LowerCutOff = 0.02
-HigherCutOff = 0.05
-FALL_THRESH_HIGH= 0.03
-FALL_THRESH_LOW = 0.025
+secondFilterLength = 70
+LowerCutOff = 0.005
+HigherCutOff = 0.006
+FALL_THRESH_HIGH= 800
+FALL_THRESH_LOW = 200
+PARSE_FALL_GROUP_LIMIT = 20
 total_nots = 0
 
 lines_fifo = deque()
+sum_data = deque()
+
 
 def init():
     parser = argparse.ArgumentParser(description='input filter for IMU data')
@@ -40,53 +44,73 @@ def main(args):
     ft2 = args.secondCutOff / args.samplingFreq
 
     for sample in xrange(0, args.filterLength):
-	if (sample != M / 2):
-	    weights.insert(sample, (math.sin(2*M_PI*ft2*(sample-M/2))/(M_PI*(sample-M/2))-math.sin(2*M_PI*ft1*(sample-M/2))/(M_PI*(sample-M/2))) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
-	else:
-	    weights.insert(sample, (2*(ft2-ft1)) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
+        if (sample != M / 2):
+            weights.insert(sample, (math.sin(2*M_PI*ft2*(sample-M/2))/(M_PI*(sample-M/2))-math.sin(2*M_PI*ft1*(sample-M/2))/(M_PI*(sample-M/2))) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
+    else:
+        weights.insert(sample, (2*(ft2-ft1)) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
 
     print "weights = %s" % weights
     fd = open(IMU_data, 'r')
 
+    dataPoint = 0
+
     while (1):
+        outputSignal = 0.0
 
-	    outputSignal = 0.0
+        line = fd.readline()
+        if line:
+            dataPoint += 1
+            outputSignal = 0.0
+            #print "lines_fifo = %s" % lines_fifo
+            list_line = line.split(',')
+            if len(lines_fifo) >= args.filterLength:
+                for sample in xrange(0, args.filterLength):
+                    #print "float(lines_fifo[sample]) = %f" % float(lines_fifo[sample])
+                    #print "weights = %f " % weights[sample]
+                    #print "float(lines_fifo = %f)" % float(lines_fifo[sample])
+                    outputSignal = outputSignal + weights[sample]*float(lines_fifo[sample])
 
-	    line = fd.readline()
-	    if line:
-		outputSignal = 0.0
-		#print "lines_fifo = %s" % lines_fifo
-		list_line = line.split(',')
-		if len(lines_fifo) >= args.filterLength:
-		    for sample in xrange(0, args.filterLength):
-			#print "float(lines_fifo[sample]) = %f" % float(lines_fifo[sample])
-			#print "weights = %f " % weights[sample]
-			#print "float(lines_fifo = %f)" % float(lines_fifo[sample])
-			outputSignal = outputSignal + weights[sample]*float(lines_fifo[sample])
+                print ("[{}] outputSignal = {}").format(dataPoint, outputSignal)
 
-		    print "outputSignal = %f" % outputSignal
+                sum_data.append(outputSignal)
+                #print ("sum_data = {}".format(sum_data))
 
-		    if outputSignal >= FALL_THRESH_HIGH or outputSignal <= FALL_THRESH_LOW:
-				print "fall detected"
-				global total_nots
-				total_nots += 1
-				# push parse notification
-				if(total_nots % 20 == 0):
-					send_push()
+                if (len(sum_data) >= secondFilterLength):
+                    total_sum_data = 0
+                    for elem in sum_data:
+                        total_sum_data +=elem
+                    if total_sum_data >= FALL_THRESH_HIGH or total_sum_data <= FALL_THRESH_LOW:
+                        print "fall detected"
+                        print ("total_sum_data = {}".format(total_sum_data))
+                        global total_nots
+                        total_nots += 1
+                        # push parse notification
+                        if(total_nots % PARSE_FALL_GROUP_LIMIT == 0):
+                            send_push()
+                    sum_data.popleft()
 
-		    lines_fifo.popleft()
-		    #print "list_line = %s" % list_line
-		    if (len(list_line) > 2):
-			lines_fifo.append(list_line[2])
-		else:
-		    list_line = line.split(',')
-		    #print "list_line = %s" % list_line
-		    if (len(list_line) > 2):
-			lines_fifo.append(list_line[2])
+                '''
+                if outputSignal >= FALL_THRESH_HIGH or outputSignal <= FALL_THRESH_LOW:
+                    print "fall detected"
+                    global total_nots
+                    total_nots += 1
+                    # push parse notification
+                    if(total_nots % 20 == 0):
+                        send_push()
+                '''
 
-	    else:
-			print "waiting for more data.."
-			time.sleep(1)
+                lines_fifo.popleft()
+                #print "list_line = %s" % list_line
+                if (len(list_line) > 2):
+                    lines_fifo.append(list_line[2])
+            else:
+                list_line = line.split(',')
+                #print "list_line = %s" % list_line
+                if (len(list_line) > 2):
+                    lines_fifo.append(list_line[2])
+        else:
+            print "waiting for more data.."
+            time.sleep(1)
 
 def send_push():
     connection.request('POST', '/1/push', json.dumps({
@@ -100,7 +124,7 @@ def send_push():
             "X-Parse-Application-Id": appID,
             "X-Parse-REST-API-Key": apiKey,
             "Content-Type": "application/json"
-        })
+            })
     result = json.loads(connection.getresponse().read())
     print result
 
