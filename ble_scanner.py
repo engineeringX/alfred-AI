fe1e33e20b3933f9ae14edd40ada66695171bde7
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2016-03-04 15:56:19 -0500

    Update ble_scanner and filterData to use multiprocessing pipes

--- ble_scanner.py
+++ ble_scanner.py
@@ -1,94 +1,112 @@
 #!/usr/bin/env python
 
 import bglib, serial, time, datetime, signal, sys
+from multiprocessing import Pipe
 
-f = open(sys.argv[1], 'w')
-packet_count = 0
+send = None
+pipe = None
+
+def pipe_send(pipe, data):
+  pipe.send(data)
+
+def file_write(f, data):
+  f.write(','.join(['%d' % b for b in data]) + "\n")
 
 # handler to notify of an API parser timeout condition
 def my_timeout(sender, args):
-    # might want to try the following lines to reset, though it probably
-    # wouldn't work at this point if it's already timed out:
-    #ble.send_command(ser, ble.ble_cmd_system_reset(0))
-    #ble.check_activity(ser, 1)
-    print "BGAPI parser timed out. Make sure the BLE device is in a known/idle state."
+  # might want to try the following lines to reset, though it probably
+  # wouldn't work at this point if it's already timed out:
+  #ble.send_command(ser, ble.ble_cmd_system_reset(0))
+  #ble.check_activity(ser, 1)
+  print "BGAPI parser timed out. Make sure the BLE device is in a known/idle state."
 
 # handler to print scan responses with a timestamp
 def my_ble_evt_gap_scan_response(sender, args):
-    t = datetime.datetime.now()
-    sender = ''.join(['%02X' % b for b in args["sender"][::-1]])
-    if(sender == "EB16450404D9"):
-        it = iter(args["data"][args["data"].index(0xff)+1:])
-        data_bytes = [((next(it) << 8) | x) for x in it]
-        data = [(x - 65536) if (x & 0x8000) else x for x in data_bytes]
-        #disp_list = []
-        #disp_list.append("%ld.%03ld" % (time.mktime(t.timetuple()), t.microsecond/1000))
-        #disp_list.append("%d" % args["rssi"])
-        #disp_list.append("%d" % args["packet_type"])
-        #disp_list.append("%s" % sender)
-        #disp_list.append("%d" % args["address_type"])
-        #disp_list.append("%d" % args["bond"])
-        #disp_list.append("%s" % ','.join(['%d' % b for b in data]))
-        f.write(','.join(['%d' % b for b in data]) + "\n")
-        global packet_count
-        packet_count += 1
-        if(packet_count >= 1500):
-            exit_handler(0, 0)
+  t = datetime.datetime.now()
+  sender = ''.join(['%02X' % b for b in args["sender"][::-1]])
+  if(sender == "EB16450404D9"):
+      it = iter(args["data"][args["data"].index(0xff)+1:])
+      data_bytes = [((next(it) << 8) | x) for x in it]
+      data = [(x - 65536) if (x & 0x8000) else x for x in data_bytes]
+      send(pipe, data)
+      #disp_list = []
+      #disp_list.append("%ld.%03ld" % (time.mktime(t.timetuple()), t.microsecond/1000))
+      #disp_list.append("%d" % args["rssi"])
+      #disp_list.append("%d" % args["packet_type"])
+      #disp_list.append("%s" % sender)
+      #disp_list.append("%d" % args["address_type"])
+      #disp_list.append("%d" % args["bond"])
+      #disp_list.append("%s" % ','.join(['%d' % b for b in data]))
+      #global packet_count
+      #packet_count += 1
+      #if(packet_count >= 1500):
+      #    exit_handler(0, 0)
 
-def main():
-    # Handle ctrl-c
-    signal.signal(signal.SIGINT, exit_handler)
+def ble_scanner(p):
+  # Set the pipe and send function
+  pipe = p
+  if hasattr(p, 'send'):
+    send = pipe_send
+  else:
+    send = file_write
 
-    # NOTE: CHANGE THESE TO FIT YOUR TEST SYSTEM
-    port_name = "/dev/ttyACM0"
-    baud_rate = 115200
-    packet_mode = True
+  # Handle ctrl-c
+  signal.signal(signal.SIGINT, exit_handler)
 
-    # create BGLib object
-    ble = bglib.BGLib()
-    ble.packet_mode = packet_mode
+  # NOTE: CHANGE THESE TO FIT YOUR TEST SYSTEM
+  port_name = "/dev/ttyACM0"
+  baud_rate = 115200
+  packet_mode = True
 
-    # add handler for BGAPI timeout condition (hopefully won't happen)
-    ble.on_timeout += my_timeout
+  # create BGLib object
+  ble = bglib.BGLib()
+  ble.packet_mode = packet_mode
 
-    # add handler for the gap_scan_response event
-    ble.ble_evt_gap_scan_response += my_ble_evt_gap_scan_response
+  # add handler for BGAPI timeout condition (hopefully won't happen)
+  ble.on_timeout += my_timeout
 
-    # create serial port object and flush buffers
-    ser = serial.Serial(port=port_name, baudrate=baud_rate, timeout=1)
-    ser.flushInput()
-    ser.flushOutput()
+  # add handler for the gap_scan_response event
+  ble.ble_evt_gap_scan_response += my_ble_evt_gap_scan_response
 
-    # disconnect if we are connected already
-    ble.send_command(ser, ble.ble_cmd_connection_disconnect(0))
-    ble.check_activity(ser, 1)
+  # create serial port object and flush buffers
+  ser = serial.Serial(port=port_name, baudrate=baud_rate, timeout=1)
+  ser.flushInput()
+  ser.flushOutput()
 
-    # stop advertising if we are advertising already
-    ble.send_command(ser, ble.ble_cmd_gap_set_mode(0, 0))
-    ble.check_activity(ser, 1)
+  # disconnect if we are connected already
+  ble.send_command(ser, ble.ble_cmd_connection_disconnect(0))
+  ble.check_activity(ser, 1)
 
-    # stop scanning if we are scanning already
-    ble.send_command(ser, ble.ble_cmd_gap_end_procedure())
-    ble.check_activity(ser, 1)
+  # stop advertising if we are advertising already
+  ble.send_command(ser, ble.ble_cmd_gap_set_mode(0, 0))
+  ble.check_activity(ser, 1)
 
-    # set scan parameters
-    ble.send_command(ser, ble.ble_cmd_gap_set_scan_parameters(0xC8, 0xC8, 1))
-    ble.check_activity(ser, 1)
+  # stop scanning if we are scanning already
+  ble.send_command(ser, ble.ble_cmd_gap_end_procedure())
+  ble.check_activity(ser, 1)
 
-    # start scanning now
-    ble.send_command(ser, ble.ble_cmd_gap_discover(1))
-    ble.check_activity(ser, 1)
+  # set scan parameters
+  ble.send_command(ser, ble.ble_cmd_gap_set_scan_parameters(0xC8, 0xC8, 1))
+  ble.check_activity(ser, 1)
 
-    while (1):
-        # check for all incoming data (no timeout, non-blocking)
-        ble.check_activity(ser)
+  # start scanning now
+  ble.send_command(ser, ble.ble_cmd_gap_discover(1))
+  ble.check_activity(ser, 1)
 
-        # don't burden the CPU
-        #time.sleep(0.01)
+  while (1):
+      # check for all incoming data (no timeout, non-blocking)
+      ble.check_activity(ser)
+
+      # don't burden the CPU
+      #time.sleep(0.01)
 
 def exit_handler(signal, frame):
-    f.close()
-    exit(0)
+  pipe.close()
+  exit(0)
 
 if __name__ == '__main__':
-    main()
+  if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as f:
+      ble_scanner(f)
+  else:
+      ble_scanner(sys.stdout)
--- filterData.py
+++ filterData.py
@@ -3,13 +3,15 @@ import math
 import argparse
 from collections import deque
 import time
+import sys
+from multiprocessing import Pipe
 
 appID = "kKW7oJS0nwEG4V6f3LvYooU5BQxFnH6eZ9aS31A3"
 apiKey = "HEZHvUyEqV4VOV61YaEFbMywGKq7pJNlPhlQtWRt"
 connection = httplib.HTTPSConnection('api.parse.com', 443)
 connection.connect()
 
-IMU_data = "../parsePi/samples.txt"
+fd = None 
 filterLength = 50
 LowerCutOff = 0.02
 HigherCutOff = 0.05
@@ -19,7 +21,19 @@ total_nots = 0
 
 lines_fifo = deque()
 
-def init():
+def pipe_recv(pipe):
+  return pipe.recv()
+
+def file_readline(f):
+  return f.readline()
+
+def init(p):
+    fd = p
+    if hasattr(p, 'recv'):
+      recv = pipe_recv
+    else:
+      recv = file_readline
+
     parser = argparse.ArgumentParser(description='input filter for IMU data')
 
     parser.add_argument('-i', action="store", dest="IMU_data")
@@ -32,77 +46,72 @@ def init():
     main(args)
 
 def main(args):
-    weights = []
-    M = args.filterLength - 1
-    #print "M = %d" % M
-    M_PI = math.pi
-    ft1 = args.firstCutOff / args.samplingFreq
-    ft2 = args.secondCutOff / args.samplingFreq
+  weights = []
+  M = args.filterLength - 1
+  #print "M = %d" % M
+  M_PI = math.pi
+  ft1 = args.firstCutOff / args.samplingFreq
+  ft2 = args.secondCutOff / args.samplingFreq
 
-    for sample in xrange(0, args.filterLength):
-	if (sample != M / 2):
-	    weights.insert(sample, (math.sin(2*M_PI*ft2*(sample-M/2))/(M_PI*(sample-M/2))-math.sin(2*M_PI*ft1*(sample-M/2))/(M_PI*(sample-M/2))) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
-	else:
-	    weights.insert(sample, (2*(ft2-ft1)) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
+  for sample in xrange(0, args.filterLength):
+    if (sample != M / 2):
+        weights.insert(sample, (math.sin(2*M_PI*ft2*(sample-M/2))/(M_PI*(sample-M/2))-math.sin(2*M_PI*ft1*(sample-M/2))/(M_PI*(sample-M/2))) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
+    else:
+        weights.insert(sample, (2*(ft2-ft1)) * (0.54 - 0.46 * math.cos(2*M_PI*sample/M)))
 
-    print "weights = %s" % weights
-    fd = open(IMU_data, 'r')
+  print "weights = %s" % weights
 
-    while (1):
+  while (1):
+    outputSignal = 0.0
+    data = recv(fd)
+	
+    #print "lines_fifo = %s" % lines_fifo
+	  if len(lines_fifo) >= args.filterLength:
+	    for sample in xrange(0, args.filterLength):
+	  	  #print "float(lines_fifo[sample]) = %f" % float(lines_fifo[sample])
+	  	  #print "weights = %f " % weights[sample]
+	  	  #print "float(lines_fifo = %f)" % float(lines_fifo[sample])
+	  	  outputSignal = outputSignal + weights[sample]*float(lines_fifo[sample])
 
-	    outputSignal = 0.0
+	    print "outputSignal = %f" % outputSignal
 
-	    line = fd.readline()
-	    if line:
-		outputSignal = 0.0
-		#print "lines_fifo = %s" % lines_fifo
-		list_line = line.split(',')
-		if len(lines_fifo) >= args.filterLength:
-		    for sample in xrange(0, args.filterLength):
-			#print "float(lines_fifo[sample]) = %f" % float(lines_fifo[sample])
-			#print "weights = %f " % weights[sample]
-			#print "float(lines_fifo = %f)" % float(lines_fifo[sample])
-			outputSignal = outputSignal + weights[sample]*float(lines_fifo[sample])
+	    if outputSignal >= FALL_THRESH_HIGH or outputSignal <= FALL_THRESH_LOW:
+	  	  print "fall detected"
+	  	  global total_nots
+	  	  total_nots += 1
+	  	
+      # push parse notification
+	  	if(total_nots % 50 == 0):
+	  		send_push()
 
-		    print "outputSignal = %f" % outputSignal
-
-		    if outputSignal >= FALL_THRESH_HIGH or outputSignal <= FALL_THRESH_LOW:
-				print "fall detected"
-				global total_nots
-				total_nots += 1
-				# push parse notification
-				if(total_nots % 50 == 0):
-					send_push()
-
-		    lines_fifo.popleft()
-		    #print "list_line = %s" % list_line
-		    if (len(list_line) > 2):
-			lines_fifo.append(list_line[2])
-		else:
-		    list_line = line.split(',')
-		    #print "list_line = %s" % list_line
-		    if (len(list_line) > 2):
-			lines_fifo.append(list_line[2])
-
-	    else:
-			print "waiting for more data.."
-			time.sleep(1)
+	    lines_fifo.popleft()
+	    #print "data = %s" % data
+	    
+      if (len(data) > 2):
+	  	  lines_fifo.append(data[2])
+	  else:
+	      #print "data = %s" % data
+	      if (len(data) > 2):
+		      lines_fifo.append(data[2])
 
 def send_push():
-    connection.request('POST', '/1/push', json.dumps({
-        "channels": [
-            "Alfred"
-            ],
-        "data": {
-            "alert": "A fall has been detected"
-            }
-        }), {
-            "X-Parse-Application-Id": appID,
-            "X-Parse-REST-API-Key": apiKey,
-            "Content-Type": "application/json"
-        })
-    result = json.loads(connection.getresponse().read())
-    print result
+  connection.request('POST', '/1/push', json.dumps({
+      "channels": [
+          "Alfred"
+          ],
+      "data": {
+          "alert": "A fall has been detected"
+          }
+      }), {
+          "X-Parse-Application-Id": appID,
+          "X-Parse-REST-API-Key": apiKey,
+          "Content-Type": "application/json"
+      })
+  result = json.loads(connection.getresponse().read())
+  print result
 
 if __name__ == "__main__":
-    init()
+  if len(sys.argv) > 1:
+    init(sys.argv[1])
+  else:
+    init(sys.stdout)
